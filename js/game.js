aa27ee7f32ee2f5a2274fe3fc83ff9bc1b91f513
brkydnc <lembervideo12@gmail.com>
2018-03-09 17:10:55 +0300

    Infinite tile loop is activated!

--- js/game.js
+++ js/game.js
@@ -1,13 +1,16 @@
 //All modifies and other things will go here
 const ROWS = [];
-for (y = -10; y < config.screen.tileCapacity.forHeight; y++) {
-    ROWS.push(new Row(y * config.tile.height, generateArray()))
-}
+for (y = -2; y < config.screen.tileCapacity.forHeight; y++) {
+    ROWS.push(new Row(y * config.tile.height, generateArray()));
+};
 function game() {
-    ROWS.forEach(row => {
-        row.drawTiles()
-        row.updateTiles()
-    })
-}
-console.log("Game is paused at the moment")
-console.log("Use 'config.game.isPaused = false'")
+    ROWS.forEach((row, index, array) => {
+        if(row.posY >= config.screen.height){
+            array.splice(index,1);
+            array.unshift(new Row(ROWS[0].posY-config.tile.height,generateArray()))
+        }else{
+            row.drawTiles()
+            row.updateTiles()
+        };
+    });
+};
