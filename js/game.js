825c3bc8059f23f8d583e837d91bf7562edb8394
brkydnc <lembervideo12@gmail.com>
2018-03-09 23:57:08 +0300

    Made options for finish

--- js/game.js
+++ js/game.js
@@ -1,16 +1,23 @@
 //All modifies and other things will go here
 const ROWS = [];
+var lastRow;
 for (y = -2; y < config.screen.tileCapacity.forHeight; y++) {
     ROWS.push(new Row(y * config.tile.height, generateArray()));
-};
+}
+updateLast(ROWS.length);
 function game() {
     ROWS.forEach((row, index, array) => {
-        if(row.posY >= config.screen.height){
-            array.splice(index,1);
-            array.unshift(new Row(ROWS[0].posY-config.tile.height,generateArray()))
-        }else{
-            row.drawTiles()
-            row.updateTiles()
-        };
+        if (row.posY >= config.screen.height) {
+            array.splice(index, 1);
+            array.unshift(
+                new Row(ROWS[0].posY - config.tile.height, generateArray())
+            );
+            if (!row.isClicked) {
+                finishGame();
+            }
+        } else {
+            row.drawTiles();
+            row.updateTiles();
+        }
     });
-};
+}
