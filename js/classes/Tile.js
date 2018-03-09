9ead77d42dc4575eb37790f59d1332e12c47d787
brkydnc <lembervideo12@gmail.com>
2018-03-09 23:56:49 +0300

    Tidied js

--- js/classes/Tile.js
+++ js/classes/Tile.js
@@ -1,17 +1,18 @@
-function Tile(_x,_y,c) {
+function Tile(_x, _y, c) {
     this.posX = _x;
     this.posY = _y;
     this.isClickable = c;
     this.isClicked = false;
+    this.isLocked = true;
     this.color;
-    (this.isClickable) ? this.color = config.tile.clickableKeyColor : this.color = config.tile.unclickableKeyColor;
+    (this.isClickable) ? (this.color = config.tile.clickableKeyColor) : (this.color = config.tile.unclickableKeyColor);
 
-    this.update = function() {
+    this.update = function () {
         this.posY += config.game.scrollSpeed;
-        (this.isClicked) ? this.color = config.tile.clickedKeyColor : null;
+        this.isClicked ? (this.color = config.tile.clickedKeyColor) : null;
     };
-    this.draw = function() {
+    this.draw = function () {
         fill(this.color);
-        rect(this.posX, this.posY, config.tile.width, config.tile.height);            
+        rect(this.posX, this.posY, config.tile.width, config.tile.height);
     };
 }
