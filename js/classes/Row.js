7903ef6c2c91a401f50b3ad1948a2f1ff0ad030b
brkydnc <lembervideo12@gmail.com>
2018-03-09 17:09:50 +0300

    Oriented rows to disappear

--- js/classes/Row.js
+++ js/classes/Row.js
@@ -1,18 +1,20 @@
-function Row(y,row) {
+function Row(_y,row) {
     (row.length != config.screen.tileCapacity.forWidth) ? console.error("Row size is not equal to screen tile capacity") : null
     this.tiles = [];
+    this.posY = _y;
     //Add tiles in this row
     for(w=0; w < config.screen.tileCapacity.forWidth; w++){
         if(row[w] === 0){
-            this.tiles.push(new Tile(config.tile.width * w, y, false))
+            this.tiles.push(new Tile(config.tile.width * w, _y, false))
         }else{
-            this.tiles.push(new Tile(config.tile.width * w, y, true))
+            this.tiles.push(new Tile(config.tile.width * w, _y, true))
         };   
     };
     this.updateTiles = function() {
         this.tiles.forEach(tile => {
             tile.update();
         });
+        this.posY += config.game.scrollSpeed
     };
     this.drawTiles = function () {
         this.tiles.forEach(tile => {
