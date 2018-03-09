61b7a63c1350d0e4b308d7017437286682cf808f
brkydnc <lembervideo12@gmail.com>
2018-03-09 23:55:30 +0300

    Tidied js

--- js/classes/Row.js
+++ js/classes/Row.js
@@ -1,24 +1,27 @@
-function Row(_y,row) {
-    (row.length != config.screen.tileCapacity.forWidth) ? console.error("Row size is not equal to screen tile capacity") : null
+function Row(_y, row) {
+    row.length != config.screen.tileCapacity.forWidth
+        ? console.error("Row size is not equal to screen tile capacity")
+        : null;
     this.tiles = [];
     this.posY = _y;
+    this.isClicked = false;
     //Add tiles in this row
-    for(w=0; w < config.screen.tileCapacity.forWidth; w++){
-        if(row[w] === 0){
-            this.tiles.push(new Tile(config.tile.width * w, _y, false))
-        }else{
-            this.tiles.push(new Tile(config.tile.width * w, _y, true))
-        };   
-    };
-    this.updateTiles = function() {
+    for (w = 0; w < config.screen.tileCapacity.forWidth; w++) {
+        if (row[w] === 0) {
+            this.tiles.push(new Tile(config.tile.width * w, _y, false));
+        } else {
+            this.tiles.push(new Tile(config.tile.width * w, _y, true));
+        }
+    }
+    this.updateTiles = function () {
         this.tiles.forEach(tile => {
             tile.update();
         });
-        this.posY += config.game.scrollSpeed
+        this.posY += config.game.scrollSpeed;
     };
     this.drawTiles = function () {
         this.tiles.forEach(tile => {
             tile.draw();
         });
     };
-};
+}
