bda3b3e6694ccdd28eeb01791ec1bdd66197f9c4
brkydnc <lembervideo12@gmail.com>
2018-03-09 18:37:57 +0300

    Click system is up

--- js/pilot/render.js
+++ js/pilot/render.js
@@ -1,22 +1,21 @@
 function setup(){
     //+1 cause of tile strokes
     createCanvas(config.screen.width+1,config.screen.height+1);
-    game()
+    game();
 };
+
 function draw() {
     if(!config.game.isPaused){
         background(255,0,255);
         game()
-    }
+    };
 };
+
 function keyPressed() {
-    if(key == "P" && config.game.isPaused){
-        config.game.isPaused = false;
-    } else if (key == "P" && !config.game.isPaused){
-        config.game.isPaused = true;
-    } else if (keyCode === 38){
-        config.game.scrollSpeed += 1
-    } else if (keyCode === 40) {
-        config.game.scrollSpeed -= 1
+    remoteGame(key);
+}
+function mousePressed(){
+    if(!config.game.isPaused){
+        clickTile();
     }
 }
