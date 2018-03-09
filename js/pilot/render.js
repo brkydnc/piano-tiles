fbe0077a88c7f420e09d4311f45b7468a94f3772
brkydnc <lembervideo12@gmail.com>
2018-03-09 23:58:16 +0300

    All the game is ready to play

--- js/pilot/render.js
+++ js/pilot/render.js
@@ -1,21 +1,28 @@
-function setup(){
+function setup() {
     //+1 cause of tile strokes
-    createCanvas(config.screen.width+1,config.screen.height+1);
+    createCanvas(config.screen.width + 1, config.screen.height + 1);
     game();
-};
+}
 
 function draw() {
-    if(!config.game.isPaused){
-        background(255,0,255);
-        game()
-    };
-};
+    if (!config.game.isPaused) {
+        background(255, 0, 255);
+        game();
+    }
+    if (config.game.isOver) {
+        finishGame();
+    }
+}
 
 function keyPressed() {
     remoteGame(key);
 }
-function mousePressed(){
-    if(!config.game.isPaused){
+
+function mousePressed() {
+    if (!config.game.isStarted) {
+        startGame();
+    }
+    if (!config.game.isPaused) {
         clickTile();
     }
 }
